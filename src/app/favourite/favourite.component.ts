2424f98f65db9e1b33167a9fe6f85d0392bfc004
Rachel Bingham <eddie901@gmail.com>
2022-12-31 17:32:42 +0000

    Two or three ways with styles. Last one takes priority. Only one is applied.

--- src/app/favourite/favourite.component.ts
+++ src/app/favourite/favourite.component.ts
@@ -3,14 +3,18 @@ import {Component, OnInit, Input, Output, EventEmitter} from '@angular/core';
 @Component({
   selector: 'favourite',
   templateUrl: './favourite.component.html',
-  styleUrls: ['./favourite.component.scss']
+  styleUrls: ['./favourite.component.scss'],
+  styles: [
+    `
+      .glyphicon {
+        color: green;
+      }
+    `
+  ]
 })
 export class FavouriteComponent {
   @Input('is-favourite') isFavourite = true;
-  @Output() change = new EventEmitter;
-
-  ngOnInit() {
-  }
+  @Output('change') change = new EventEmitter;
 
   onClick() {
     this.isFavourite = !this.isFavourite;
