eb9977b6e84e30fc617b6e6f8b0db24b4e43a2bf
Rachel Bingham <eddie901@gmail.com>
2023-01-01 17:02:57 +0000

    Two ways for style bindings - direct or using ngStyle

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,17 +6,5 @@ import {Component} from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  courses: any;
-
-  loadCourses() {
-    this.courses = [
-      {id: 1, name: "course 1 "},
-      {id: 2, name: "course 2 "},
-      {id: 3, name: "course 3 "}
-    ]
-  }
-
-  trackCourse(index: any, course: any) {
-    return course ? course.id : undefined;
-  }
+  canSave = true;
 }
