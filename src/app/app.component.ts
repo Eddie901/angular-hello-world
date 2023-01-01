0dac06700c9c5788419b83eb9d240384a69ede05
Rachel Bingham <eddie901@gmail.com>
2023-01-01 16:28:59 +0000

    Change detection onRemove

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,8 +7,17 @@ import {Component} from '@angular/core';
 })
 export class AppComponent {
   courses = [
-    { id: 1, name: "course 1 "},
-    { id: 2, name: "course 2 "},
-    { id: 3, name: "course 3 "}
+    {id: 1, name: "course 1 "},
+    {id: 2, name: "course 2 "},
+    {id: 3, name: "course 3 "}
   ]
+
+  onAdd() {
+    this.courses.push({id: 4, name: "course 4"})
+  }
+
+  onRemove(course: any) {
+    let index = this.courses.indexOf(course);
+    this.courses.splice(index, 1);
+  }
 }
