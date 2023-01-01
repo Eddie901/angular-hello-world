b1a83359b2992cb26549da043ce1854e9eb12a4d
Rachel Bingham <eddie901@gmail.com>
2023-01-01 16:53:45 +0000

    Using ngClass for class attribute binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import {FormsModule} from '@angular/forms';
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {CoursesComponent} from "./courses.component";
+import {FavouriteComponent} from "./favourite/favourite.component";
 
 
 @NgModule({
   declarations: [
     AppComponent,
     CoursesComponent,
+    FavouriteComponent,
   ],
   imports: [
     BrowserModule,
